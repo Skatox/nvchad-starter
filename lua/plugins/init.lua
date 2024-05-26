e64e0cef32745339bcea2b365e83300c308978a7
skatox <migueluseche@skatox.com>
2024-05-26 13:04:18 -0500

    Adding lazygit

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -72,6 +72,29 @@ return {
   --   end,
   -- },
   {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("lazygit")
+    end,
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+  {
     "coffebar/neovim-project",
     opts = {
       projects = { -- define project roots
@@ -97,7 +120,10 @@ return {
   },
   { "dyng/ctrlsf.vim" },
   { "Yggdroot/indentLine"},
-  { "inkarkat/vim-ReplaceWithRegister" },
+  { 
+    "inkarkat/vim-ReplaceWithRegister",
+    lazy = false,
+  },
   {
     "mg979/vim-visual-multi",
     branch = "master",
