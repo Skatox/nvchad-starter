c98776bf992014c13c1c951258b43d76f20bf544
skatox <migueluseche@skatox.com>
2024-08-02 22:25:23 -0500

    fix: sorting plugin config

--- lua/configs/hop-config.lua
+++ lua/configs/hop-config.lua
@@ -1,5 +1,19 @@
 -- Changing the default f keyword
-vim.api.nvim_set_keymap('', '<Leader>f', "<cmd>lua require'hop'.hint_char1()<cr>", {})
+-- vim.api.nvim_set_keymap('', '<Leader>f', "<cmd>lua require'hop'.hint_char1()<cr>", {})
 
 -- Pattern Matching with t keyword
-vim.api.nvim_set_keymap('n', '<Leader>t', "<cmd>HopPattern<CR>", {noremap = true})
+-- vim.api.nvim_set_keymap('n', '<Leader>t', "<cmd>HopPattern<CR>", {noremap = true})
+local hop = require('hop')
+local directions = require('hop.hint').HintDirection
+vim.keymap.set('', 'f', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 'F', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+end, {remap=true})
+vim.keymap.set('', 't', function()
+  hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+end, {remap=true})
+vim.keymap.set('', 'T', function()
+  hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+end, {remap=true})
