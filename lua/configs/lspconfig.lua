2f7bfd4432a0de2931ad79a4d95086186ab72420
Miguel Useche <miguel.useche@skillshare.com>
2024-10-17 11:29:17 -0500

    update to 2.5

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,4 +3,20 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "html", "cssls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+-- typescript
+lspconfig.ts_ls.setup {
+  on_attach = on_attach,
+  on_init = on_init,
+  capabilities = capabilities,
+}
+lspconfig.vuels.setup{}
+lspconfig.intelephense.setup{}
