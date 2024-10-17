66d11c7eabffd00a54b2b81d13aeeaa32ff72e0e
Miguel Useche <miguel.useche@skillshare.com>
2024-10-17 11:29:17 -0500

    update to 2.5

--- init.lua
+++ init.lua
@@ -40,6 +40,8 @@ end)
 vim.opt.relativenumber = true
 vim.g.neovide_scale_factor = 2.0
 vim.g.neovide_input_macos_alt_is_meta = true
+vim.g.copilot_node_command = "~/.nvm/versions/node/v23.0.0/bin/node"
+
 
 vim.opt.termguicolors = true
 
