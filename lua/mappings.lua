ecc6f46a750b01ffcac169d15e90e59b8cadc280
skatox <migueluseche@skatox.com>
2024-10-26 14:18:03 -0500

    updating to nerdtree

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,7 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("n", "<F4>", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
