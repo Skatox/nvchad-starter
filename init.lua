37f9602a64aa8434821f2d9d227c770daf88c538
Miguel Useche <miguel.useche@skillshare.com>
2024-12-03 09:41:24 -0500

    updating neovide macos option

--- init.lua
+++ init.lua
@@ -39,7 +39,7 @@ end)
 -- Miguel's customizations
 vim.opt.relativenumber = true
 vim.g.neovide_scale_factor = 2.0
-vim.g.neovide_input_macos_alt_is_meta = true
+vim.g.neovide_input_macos_option_key_is_meta = true
 vim.g.copilot_node_command = "~/.nvm/versions/node/v23.0.0/bin/node"
 
 
