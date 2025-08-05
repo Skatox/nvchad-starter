d9d58117378264debd66da5a343b1deafb9c4e4d
Miguel Useche <miguel.useche@skillshare.com>
2025-08-05 11:37:26 -0500

    chore: add plame plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -89,6 +89,17 @@ return {
       { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
   },
+  {
+    "f-person/git-blame.nvim",
+    -- load the plugin at startup
+    event = "VeryLazy",
+    opts = {
+        enabled = false,
+        message_template = " <summary> • <date> • <author> • <<sha>>", -- template for the blame message, check the Message template section for more options
+        date_format = "%m-%d-%Y %H:%M:%S", -- template for the date, check Date format section for more options
+        virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+    },
+  },
   { "mhinz/vim-signify" },
   -- Task Manager
   {
