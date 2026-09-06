061d33a33a45dfafb3adbd2f84b33a03bdbeddd6
skatox <migueluseche@skatox.com>
2026-09-06 13:49:03 -0500

    feat: updated plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -108,6 +108,7 @@ return {
       virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
     },
   },
+  use {'kevinhwang91/nvim-hlslens'},
   { "mhinz/vim-signify" },
   {
     "sindrets/diffview.nvim",
@@ -160,6 +161,31 @@ return {
         copy = "<C-y>"             -- Normal mode keymap to copy the CSS values between {}
       }
     }
+  },
+  {
+    'mawkler/modicator.nvim',
+    dependencies = 'mawkler/onedark.nvim', -- Add your colorscheme plugin here
+    init = function()
+      -- These are required for Modicator to work
+      vim.o.cursorline = true
+      vim.o.number = true
+      vim.o.termguicolors = true
+    end,
+    opts = {
+      -- Warn if any required option above is missing. May emit false positives
+      -- if some other plugin modifies them, which in that case you can just
+      -- ignore. Feel free to remove this line after you've gotten Modicator to
+      -- work properly.
+      show_warnings = true,
+    }
+  },
+  {
+    'Wansmer/treesj',
+    keys = { '<space>m', '<space>j', '<space>s' },
+    dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
+    config = function()
+      require('treesj').setup({--[[ your config ]]})
+    end,
   },
   {
     "mfussenegger/nvim-dap",
